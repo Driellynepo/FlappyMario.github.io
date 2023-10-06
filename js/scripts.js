16e9d538077303dd3d989f71b5ab8972401fc62f
Andrynepo <andrielly_2@live.com>
2023-10-05 22:14:07 -0300

    1.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,76 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const startButton = document.querySelector('.start-button');
+const restartButton = document.querySelector('.restart-button');
+const logo = document.querySelector('.logo');
+const gameOver = document.querySelector('.game-over');
+const ground = document.querySelector('.ground');
+const instruction = document.querySelector('.instruction')
+let score = 0;
 
-const jump = () => {
-    mario.classList.add('jump');
+startButton.addEventListener('click', () => {
+    startGame();
+})
 
-    setTimeout(() => {
-        mario.classList.remove('jump');
-    }, 500);
-}
+restartButton.addEventListener('click', () => {
+    restartGame();
+});
 
-const loop = setInterval(() => {
+function startGame() {
+    const jump = () => {
+        mario.classList.add('jump');
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`
-
-        mario.style.animation = ' none';
-        mario.style.bottom = `${marioPosition}px`;
-
-        mario.src = './images/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '50px';
-
-        clearInterval(loop);
-
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
     }
 
-}, 10);
-document.addEventListener('keydown', jump);
+    pipe.style.animation = 'none';
+
+    const loop = setInterval(() => {
+
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 160) {
+
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`
+
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPosition}px`;
+
+            mario.src = './images/mario-game-over.png';
+            mario.style.width = '75px';
+            mario.style.marginLeft = '50px';
+
+            clearInterval(loop);
+
+            gameOver.style.display = 'block';
+            restartButton.style.display = 'block';
+
+        } else {
+            score++;
+            document.querySelector('.score').textContent = `Score: ${score}`;
+        }
+
+    }, 10);
+
+    document.addEventListener('keydown', (event) => {
+        if (event.keyCode === 32) {
+            jump();
+        }
+    });
+
+    setTimeout(() => {
+        pipe.style.animation = 'pipe-animation 1.7s infinite linear';
+        ground.style.animation = 'ground-animation 0.3s infinite linear';
+    }, 0);
+
+    instruction.style.display = 'none';
+    logo.style.display = 'none';
+    startButton.style.display = 'none';
+}
+function restartGame() {
+    location.reload();
+}
